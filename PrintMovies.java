432a33141a33651abf3db7e39e00829cd76487bb
Hasan Zahid <guszahidha@student.gu.se>
2025-02-06 09:15:30 +0100

    added comments  PrintMovies.c in Java

--- PrintMovies.java
+++ PrintMovies.java
@@ -1,30 +1,31 @@
-import java.util.Scanner;
+import java.util.Scanner; // importing scanner
 
 public class PrintMovies {
 
     public static void main(String[] args){
-        Scanner userInput = new Scanner(System.in);
 
-        String[] movies;
-        int movieNum;
+        Scanner userInput = new Scanner(System.in); // creating scanner object
 
-        System.out.println("How many movie you want to store?: ");
-        movieNum = userInput.nextInt();
-        userInput.nextLine();
+        String[] movies; // declaring string array
+        int movieNum; // decalring varibale to store movie number
 
-        movies = new String[movieNum];
+        System.out.println("How many movie you want to store?: "); // print line for user
+        movieNum = userInput.nextInt(); // reads the int and stores in the variable
+        userInput.nextLine(); // clears the buffer
 
-        for(int i = 0; i < movieNum; i++){
-            System.out.println("Enter the name of movie number" + (i+1) + ": ");
-            movies[i] = userInput.nextLine();
+        movies = new String[movieNum]; // initializing the movie string array
+
+        for(int i = 0; i < movieNum; i++){ // itirating through the movie array
+            System.out.println("Enter the name of movie number" + (i+1) + ": "); // print line for the user
+            movies[i] = userInput.nextLine(); // storing the movie name in the array
         }
 
-        System.out.println("\nYour favourite movies are: ");
-        for(String favMovies : movies){
-            System.out.println(favMovies);
+        System.out.println("\nYour favourite movies are: "); // print line for user
+        for(String favMovies : movies){ // itirating through the movies array
+            System.out.println(favMovies); // printing out the movies
         }
 
-        userInput.close();
+        userInput.close(); // closing the scanner
 
     }
     
