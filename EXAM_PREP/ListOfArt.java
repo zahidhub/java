3beee67dd09fd6e279187f5367fa84d70f052f4c
Hasan Zahid <guszahidha@student.gu.se>
2025-02-19 23:52:00 +0100

    ListOfArt.java comments added in exam_prep/java

--- EXAM_PREP/ListOfArt.java
+++ EXAM_PREP/ListOfArt.java
@@ -1,38 +1,38 @@
 package EXAM_PREP;
 
-import java.util.Scanner;
+import java.util.Scanner; // importing the scanner
 
 public class ListOfArt {
     public static void main(String[] args){
 
-        Scanner userInput = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in); // crating the scanner file
 
-        String fName1;
-        String lName1;
-        String fName2;
-        String lName2;
-        String fName3;
-        String lName3;
+        String fName1; //declaring variable
+        String lName1; //declaring variable
+        String fName2; //declaring variable
+        String lName2; //declaring variable
+        String fName3; //declaring variable
+        String lName3; //declaring variable
 
-        System.out.println("Enter your 1st artist First Name: ");
-        fName1 = userInput.nextLine();
-        System.out.println("Enter your 1st artist Last Name: ");
-        lName1 = userInput.nextLine();
+        System.out.println("Enter your 1st artist First Name: ");  // printlne foe the user
+        fName1 = userInput.nextLine(); //store the name of the artist
+        System.out.println("Enter your 1st artist Last Name: "); // printlne foe the user
+        lName1 = userInput.nextLine(); //store the name of the artist
 
-        System.out.println("Enter your 2nd artist First Name: ");
-        fName2 = userInput.nextLine();
-        System.out.println("Enter your 2nd artist Last Name: ");
-        lName2 = userInput.nextLine();
+        System.out.println("Enter your 2nd artist First Name: "); // printlne foe the user
+        fName2 = userInput.nextLine(); //store the name of the artist
+        System.out.println("Enter your 2nd artist Last Name: "); // printlne foe the user
+        lName2 = userInput.nextLine(); //store the name of the artist
 
-        System.out.println("Enter your 3rd artist First Name: ");
-        fName3 = userInput.nextLine();
-        System.out.println("Enter your 3rd artist Last Name: ");
-        lName3 = userInput.nextLine();
+        System.out.println("Enter your 3rd artist First Name: "); // printlne foe the user
+        fName3 = userInput.nextLine(); //store the name of the artist
+        System.out.println("Enter your 3rd artist Last Name: "); // printlne foe the user
+        lName3 = userInput.nextLine(); //store the name of the artist
 
-        System.err.printf("%8.8S, %s %n", lName1, fName1);
-        System.err.printf("%8.8S, %s %n", lName2, fName2);
-        System.err.printf("%8.8S, %s %n", lName3, fName3);
+        System.err.printf("%8.8S, %s %n", lName1, fName1); // print the artist names in format
+        System.err.printf("%8.8S, %s %n", lName2, fName2); // print the artist names in format
+        System.err.printf("%8.8S, %s %n", lName3, fName3); // print the artist names in format
 
-        userInput.close();
+        userInput.close(); // close the scanner
     }
 }
